daf575c761fb36b778b035f6a88b2d3f278108ce
Colin Perry <123716435+colinperry07@users.noreply.github.com>
2025-11-07 08:04:56 -0500

    Refactor dungeon generation for dynamic room count
    Updated dungeon generation logic to use a random number of rooms and improved room placement checks to avoid overlap.

--- dungeon.gd
+++ dungeon.gd
@@ -1,44 +1,68 @@
 extends Node2D
 
+
 export var level = 0
 onready var tilemap = $TileMap
 
 
 func _ready():
 	randomize()
-	var room_num = [5,6].pick_random() + level
+	var n_rooms = [5,6].pick_random() + level
 	var grid_size = Vector2(64,38)
-	if room_num > grid_size.x * grid_size.y:
-		room_num = grid_size.x * grid_size.y
-	generate_dungeon(grid_size, room_num)
+	generate_dungeon(grid_size, n_rooms)
 
 
 func generate_dungeon(grid_size : Vector2, room_num : int):
-	
 	var room_coords = []
+	var room_centers = []
 	
-	for _i in range(room_num):
+	for _i in range(room_num): # generates rooms
 		var room_size = Vector2(10,10)
-		var room_viable = false
-		var starting_point
-		while !room_viable:
+		var room_starting_point
+		
+		while true:
 			var roomx = randi() % int(grid_size.x - room_size.x)
 			var roomy = randi() % int(grid_size.y - room_size.y)
-			var buffer = 1
-			starting_point = Vector2(roomx, roomy)
-			if room_coords != []:
-				for room_pos in room_coords: # added 1 pixel of extra padding to prevent any rooms from touching
-					if (starting_point.x < room_pos.x + room_size.x + buffer and starting_point.x > room_pos.x - room_size.x - buffer -1 ) or \
-					(starting_point.y < room_pos.y + room_size.y + buffer and starting_point.y > room_pos.y - room_size.y - buffer - 1):
-						pass
-					else:
-						room_viable = true
-			else:
-				room_viable = true
-		print(starting_point)
+			room_starting_point = Vector2(roomx, roomy)
+			
+			var overlap = false
+			for room_pos in room_coords:
+				if Rect2(room_starting_point, room_size).intersects(Rect2(room_pos, room_size).grow(2)):
+					overlap = true
+					break
+			
+			if !overlap:
+				break
+		for x in range(room_size.x):
+			for y in range(room_size.y):
+				tilemap.set_cell(x+room_starting_point.x, y+room_starting_point.y, 1)
 		
-		for y in range(room_size.y):
-			for x in range(room_size.x): 
-				tilemap.set_cell(x+starting_point.x, y+starting_point.y, 1)
+		room_coords.append(room_starting_point)
+		room_centers.append(room_starting_point + (room_size / 2))
 		
-		room_coords.append(starting_point)
+	for i in range(1, room_centers.size()): # generates corridors
+		create_corridor(room_centers[i-1], room_centers[i], room_coords)
+
+
+func create_corridor(from: Vector2, to: Vector2, room_coords : Array):
+	
+	var x1 = int(from.x)
+	var x2 = int(to.x)
+	
+	var y1 = int(from.y)
+	var y2 = int(to.y)
+	
+	for x in range(min(x1, x2), max(x1, x2)):
+		tilemap.set_cell(x, y1, 1)
+	for y in range(min(y1, y2), max(y1, y2) + 1):
+		tilemap.set_cell(x2, y, 1)
+	check_surroundings(Vector2(0,0), room_coords)
+
+
+func check_surroundings(point : Vector2, room_coords : Array):
+	var path_cell_locations = tilemap.get_used_cells_by_id(1)
+	
+	for room in room_coords: # removes the room cell positions from the array to isolate the path cell positions
+		for x in range(10):
+			for y in range(10):
+				path_cell_locations.erase(Vector2(room.x+x, room.y+y))
